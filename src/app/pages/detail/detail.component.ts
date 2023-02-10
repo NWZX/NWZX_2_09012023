a07af6e81bbd2140a859411fe102520a6e076fb7
NWZX <nwzx@users.noreply.github.com>
2023-02-10 11:02:20 +0100

    [Detail] add unsubscribe & clean up file

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subscription } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
@@ -11,7 +11,12 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class DetailComponent implements OnInit {
   public olympics$: Observable<Olympic[] | null> = of(null);
-  public results: any[] = [];
+  private subcription?: Subscription;
+  
+  public results: {
+    name: string;
+    series: { name: string; value: number }[];
+  }[] = [];
   public countryName: string = '';
   public numberOfEntries: number = 0;
   public numberOfMedals: number = 0;
@@ -24,18 +29,16 @@ export class DetailComponent implements OnInit {
   ) {}
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
-    this.olympics$.subscribe((data) => {
-      if (data) {
-        if (this.route.snapshot.params['id']) {
-          const result = data.find(
-            (item) => item.id.toString() === this.route.snapshot.params['id']
-          );
-          if (result) {
-            console.log(result);
-            
-            this.countryName = result.country;
-            this.numberOfEntries = result.participations.length;
-            this.results = [{
+    this.subcription = this.olympics$.subscribe((data) => {
+      if (data && this.route.snapshot.params['id']) {
+        const result = data.find(
+          (item) => item.id.toString() === this.route.snapshot.params['id']
+        );
+        if (result) {
+          this.countryName = result.country;
+          this.numberOfEntries = result.participations.length;
+          this.results = [
+            {
               name: result.country,
               series: result.participations.map((item) => {
                 this.numberOfMedals += item.medalsCount;
@@ -44,24 +47,15 @@ export class DetailComponent implements OnInit {
                   name: item.year.toString(),
                   value: item.medalsCount,
                 };
-              })
-            }]
-          }
+              }),
+            },
+          ];
         }
       }
     });
   }
-
-  view: [number, number] = [700, 400];
-
-  // options
-  legendPosition: string = 'below';
-  tooltipText: () => void = () => {};
-
-  scheme = 'cool';
-
-  onSelect(data: any): void {
-    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  ngOnDestory(): void {
+    this.subcription?.unsubscribe();
   }
 
   onBack(): void {
